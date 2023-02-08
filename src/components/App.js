a04b914944851145a269a1a8c74c65f755ba2c3b
Giovanna <giovannad1clonius@gmail.com>
2023-02-08 14:24:44 -0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+import Corpo from "./Corpo";
+import Posts from "./Posts";
+import SideBar from "./SideBar";
+import Stories from "./Stories";
+import Sugestoes from "./Sugestoes";
+import Usuario from "./Usuario";
+
+export default function App() {
+    return (
+        <NavBar />
+    )
+}
