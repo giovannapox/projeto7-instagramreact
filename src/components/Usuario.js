b45177f6c1db9791aab46f4b86af834b62deeceb
Giovanna <giovannad1clonius@gmail.com>
2023-02-10 12:33:09 -0300

    Usuario e foto implementados com useState

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,13 +1,37 @@
+import { useState } from "react";
+
 export default function Usuario() {
+    const [nomeDoUsuario, setNomeDoUsuario] = useState("catanacomics")
+    const [img, setImg] = useState("assets/img/catanacomics.svg");
+
+    function trocarFoto() {
+        let novaImg = prompt("Insira o link da imagem");
+        if(novaImg === null || novaImg === ""){
+            setImg(img)
+        } else {
+        setNomeDoUsuario(novaImg);
+        }
+    }
+
+    function trocarNome() {
+        let novoNome = prompt("Insira seu novo nome de Usuário");
+        if(novoNome === null || novoNome === ""){
+            setNomeDoUsuario(nomeDoUsuario)
+        } else {
+        setNomeDoUsuario(novoNome);
+        }
+    }
+
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img onClick={trocarFoto} src={img} alt="imagem de perfil" />
             <div class="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{nomeDoUsuario}</strong>
+                    <ion-icon onClick={trocarNome} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
     )
 }
+// (condição) ? true : false
